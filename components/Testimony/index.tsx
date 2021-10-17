0f97f3e05d9d0d1d7c73e4d0dcb4cc3fda18aca7
Rigalli Pablo <rigallipablo@gmail.com>
2021-10-17 12:35:31 -0300

    Add animation

--- components/Testimony/index.tsx
+++ components/Testimony/index.tsx
@@ -2,18 +2,37 @@ import React, { FC } from 'react';
 import styles from './Testimony.module.css';
 import { TestimonyTypes } from '../../types/Model';
 import Card from './Card';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const Testimony: FC<TestimonyTypes> = ({ title, description }) => {
+  const [ref, inView, entry] = useInView({
+    /* Optional options */
+    threshold: 0.5,
+    triggerOnce: false
+  });
+  const variants = {
+    visible: { opacity: 1, y: -30 },
+    hidden: {
+      opacity: 0,
+      y: 50
+    }
+  };
+
   return (
-    <div className={styles.testimony}>
-      <div className={styles.testimony_container}>
-        <div className={styles.testimony__header}>
+    <motion.div className={styles.testimony} ref={ref}>
+      <motion.div
+        className={styles.testimony_container}
+        animate={inView ? 'visible' : 'hidden'}
+        variants={variants}
+        transition={{ duration: 3, ease: 'easeOut' }}>
+        <motion.div className={styles.testimony__header}>
           <h2 dangerouslySetInnerHTML={{ __html: title }} />
           <p dangerouslySetInnerHTML={{ __html: description }} />
-        </div>{' '}
+        </motion.div>{' '}
         <Card />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
